e092a3aef64d68a270c9b6c4dd7d79d6be4034d0
saurav kumar <seoneseven@gmail.com>
2025-02-02 13:49:52 +0000

    .

--- generator.cpp
+++ generator.cpp
@@ -1,88 +1,17 @@
-// #include "testlib.h"
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math,inline")
 #include <bits/stdc++.h>
 using namespace std;
 
-#if defined(LOCAL)
-#include "util.h"
-#else
-#define clg(...)
-#endif
+using ll = int64_t;
+using ull = uint64_t;
+using uint = uint32_t;
+using i8 = int8_t;
+using u8 = uint8_t;
+using ish = int16_t;
+using ush = uint16_t;
 
-inline namespace IO {
-#define SFINAE(x, ...)																					   \
-    template <class, class = void> struct x : std::false_type {};											\
-    template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
-
-SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>()));
-SFINAE(DefaultO, decltype(std::cout << std::declval<T &>()));
-SFINAE(IsTuple, typename std::tuple_size<T>::type);
-SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
-
-template <auto &is> struct Reader {
-    template <class T> void Impl(T &t) {
-        if constexpr (DefaultI<T>::value)
-            is >> t;
-        else if constexpr (Iterable<T>::value) {
-            for (auto &x : t)
-                Impl(x);
-        } else if constexpr (IsTuple<T>::value) {
-            std::apply([this](auto &...args) { (Impl(args), ...); }, t);
-        } else
-            static_assert(IsTuple<T>::value, "No matching type for read");
-    }
-    template <class... Ts> void read(Ts &...ts) { ((Impl(ts)), ...); }
-};
-
-template <class... Ts> void re(Ts &...ts) { Reader<cin>{}.read(ts...); }
-
-template <auto &os, bool debug, bool print_nd> struct Writer {
-    string comma() const { return debug ? "," : ""; }
-    template <class T> constexpr char Space(const T &) const {
-        return print_nd && (Iterable<T>::value or IsTuple<T>::value) ? '\n' : ' ';
-    }
-    template <class T> void Impl(T const &t) const {
-        if constexpr (DefaultO<T>::value)
-            os << t;
-        else if constexpr (Iterable<T>::value) {
-            if (debug)
-                os << '{';
-            int i = 0;
-            for (auto &&x : t)
-                ((i++) ? (os << comma() << Space(x), Impl(x)) : Impl(x));
-            if (debug)
-                os << '}';
-        } else if constexpr (IsTuple<T>::value) {
-            if (debug)
-                os << '(';
-            std::apply(
-                [this](auto const &...args) {
-                    int i = 0;
-                    (((i++) ? (os << comma() << " ", Impl(args)) : Impl(args)), ...);
-                },
-                t);
-            if (debug)
-                os << ')';
-        } else
-            static_assert(IsTuple<T>::value, "No matching type for print");
-    }
-    template <class T> void ImplWrapper(T const &t) const {
-        if (debug)
-            os << "\033[0;31m";
-        Impl(t);
-        if (debug)
-            os << "\033[0m";
-    }
-    template <class... Ts> void print(Ts const &...ts) const { ((Impl(ts)), ...); }
-    template <class F, class... Ts>
-    void print_with_sep(const std::string &sep, F const &f, Ts const &...ts) const {
-        ImplWrapper(f), ((os << sep, ImplWrapper(ts)), ...), os << '\n';
-    }
-    void print_with_sep(const std::string &) const { os << '\n'; }
-};
-
-template <class... Ts> void pr(Ts const &...ts) { Writer<cout, false, true>{}.print(ts...); }
-template <class... Ts> void ps(Ts const &...ts) { Writer<cout, false, true>{}.print_with_sep(" ", ts...); }
-} // namespace IO
+const ll MOD = 1e9 + 7;
+const ll OMOD = 998'244'353;
 
 template <typename T = long long> constexpr T INF = numeric_limits<T>::max();
 
@@ -98,13 +27,11 @@ enum class Order { None, Increasing, Decreasing };
 #define rall(v) v.rbegin(), v.rend()
 #define sz(x) int(x.size())
 #define ll long long
-
-// Random number generator
 #define accuracy chrono::steady_clock::now().time_since_epoch().count()
 mt19937_64 rng(accuracy);
 
 // Generator functions namespace
-namespace test_case_generator {
+inline namespace test_case_generator {
 
 // Generate a random integer of type T between `l` and `r`
 template <typename T = long long> T gen_num(T l = -INF<T>, T r = INF<T>) {
@@ -207,7 +134,7 @@ template <typename T = long long> vector<T> gen_permutation(int len = 0) {
     assert(len >= 0 && len <= 5e6);
     vector<T> perm(len);
     iota(perm.begin(), perm.end(), 1);
-    shuffle(perm.begin(), perm.end());
+    shuffle(perm.begin(), perm.end(), rng);
     return perm;
 }
 
@@ -434,34 +361,80 @@ template <typename T = long long> vector<T> partition(T sum, T parts, T minRange
     return partition;
 }
 } // namespace test_case_generator
-using namespace test_case_generator;
 
-template <typename T = int> ostream &operator<<(ostream &other, const vector<pair<T, T>> &v) {
-    for (const auto &x : v)
-        other << x.first << ' ' << x.second << '\n';
-    return other;
-}
+inline namespace IO {
+#define SFINAE(x, ...)                                                                                       \
+    template <class, class = void> struct x : std::false_type {};                                            \
+    template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
 
-template <typename T = int> ostream &operator<<(ostream &other, const vector<T> &v) {
-    for (const auto &x : v) {
-        other << x;
-        if (x != v.back())
-            other << ' ';
+SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>()));
+SFINAE(DefaultO, decltype(std::cout << std::declval<T &>()));
+SFINAE(IsTuple, typename std::tuple_size<T>::type);
+SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
+
+template <auto &is> struct Reader {
+    template <class T> void Impl(T &t) {
+        if constexpr (DefaultI<T>::value)
+            is >> t;
+        else if constexpr (Iterable<T>::value) {
+            for (auto &x : t)
+                Impl(x);
+        } else if constexpr (IsTuple<T>::value) {
+            std::apply([this](auto &...args) { (Impl(args), ...); }, t);
+        } else
+            static_assert(IsTuple<T>::value, "No matching type for read");
     }
-    return other;
-}
+    template <class... Ts> void read(Ts &...ts) { ((Impl(ts)), ...); }
+};
 
-// complete this function according to the requirements
-void generate_test() { 
+template <class... Ts> void re(Ts &...ts) { Reader<cin>{}.read(ts...); }
+
+template <auto &os, bool print_nd> struct Writer {
+    template <class T> constexpr char Space(const T &) const {
+        return print_nd && (Iterable<T>::value or IsTuple<T>::value) ? '\n' : ' ';
+    }
+    template <class T> void Impl(T const &t) const {
+        if constexpr (DefaultO<T>::value)
+            os << t;
+        else if constexpr (Iterable<T>::value) {
+            int i = 0;
+            for (auto &&x : t)
+                ((i++) ? (os << Space(x), Impl(x)) : Impl(x));
+        } else if constexpr (IsTuple<T>::value) {
+            std::apply(
+                [this](auto const &...args) {
+                    int i = 0;
+                    (((i++) ? (os << " ", Impl(args)) : Impl(args)), ...);
+                },
+                t);
+        } else
+            static_assert(IsTuple<T>::value, "No matching type for print");
+    }
+    template <class T> void ImplWrapper(T const &t) const { Impl(t); }
+    template <class... Ts> void print(Ts const &...ts) const { ((Impl(ts)), ...); }
+    template <class F, class... Ts>
+    void print_with_sep(const std::string &sep, F const &f, Ts const &...ts) const {
+        ImplWrapper(f), ((os << sep, ImplWrapper(ts)), ...), os << '\n';
+    }
+    void print_with_sep(const std::string &) const { os << '\n'; }
+};
+
+template <class... Ts> void pr(Ts const &...ts) { Writer<cout, true>{}.print(ts...); }
+template <class... Ts> void ps(Ts const &...ts) { Writer<cout, true>{}.print_with_sep(" ", ts...); }
+} // namespace IO
+
+void gen_test() {
     
 }
 
 int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     freopen("input.txt", "w", stdout);
     int tc = 1;
-    // ps(tc);
+    // cout << tc << endl;
     for (int i = 1; i <= tc; ++i) {
-        generate_test();
+        gen_test();
     }
     return 0;
 }
