df0b8902528842566645fa4c4fdcb75f21ee2613
saurav-77 <srvkrrr@gmail.com>
2025-03-27 23:41:39 +0000

    .

--- _debug.h
+++ _debug.h
@@ -1,338 +1,119 @@
-#define writer_out cerr
-
+// #define cerr cout
 namespace __DEBUG_UTIL__ {
-    ostream &operator<<(ostream &os, __uint128_t num) {
-        std::string str;
-        do
-            str += static_cast<char>(num % 10 + 48), num /= 10;
-        while (num > 0);
-        std::reverse(str.begin(), str.end());
-        return os << str;
-    }
-
-    ostream &operator<<(ostream &os, __int128_t num) {
-        const bool neg = num < 0;
-        std::string str;
-        if (num < LONG_MIN) {
-            str.push_back((-(num % 10)) + 48);
-            num /= 10;
+    using namespace std;
+    template <typename T>
+    concept is_iterable =
+        requires(T &&x) { begin(x); } && !is_same_v<remove_cvref_t<T>, string>;
+    void print(const char *x) { cerr << x; }
+    void print(char x) { cerr << "\'" << x << "\'"; }
+    void print(bool x) { cerr << (x ? "True" : "False"); }
+    void print(const __uint128_t x) {
+        constexpr uint64_t d19 = 10'000'000'000'000'000'000U;
+        if (x > d19) {
+            cerr << uint64_t(x / d19) << setfill('0') << setw(19)
+                 << uint64_t(x % d19);
+        } else {
+            cerr << uint64_t(x);
         }
-        if (neg)
-            num = -num;
-        do
-            str += static_cast<char>(num % 10 + 48), num /= 10;
-        while (num > 0);
-        if (neg)
-            str += '-';
-        std::reverse(str.begin(), str.end());
-        return os << str;
     }
-
-    void print(__int128_t x) { writer_out << x; }
-    void print(__uint128_t x) { writer_out << x; }
-    /* Primitive Datatypes Print */
-    void print(const char *x) { writer_out << x; }
-    void print(bool x) { writer_out << (x ? "True" : "False"); }
-    void print(char x) { writer_out << '\'' << x << '\''; }
-    void print(signed short int x) { writer_out << x; }
-    void print(unsigned short int x) { writer_out << x; }
-    void print(signed int x) { writer_out << x; }
-    void print(unsigned int x) { writer_out << x; }
-    void print(signed long int x) { writer_out << x; }
-    void print(unsigned long int x) { writer_out << x; }
-    void print(signed long long int x) { writer_out << x; }
-    void print(unsigned long long int x) { writer_out << x; }
-    void print(float x) { writer_out << x; }
-    void print(double x) { writer_out << x; }
-    void print(long double x) { writer_out << x; }
-    void print(string x) { writer_out << '\"' << x << '\"'; }
-    template <size_t N>
-    void print(bitset<N> x) { writer_out << x; }
-    void print(vector<bool> v) { /* Overloaded this because stl optimizes
-                                    vector<bool> by using _Bit_reference instead of
-                                    bool to conserve space. */
+    void print(const __int128_t x) {
+        if (x >= 0) {
+            print(__uint128_t(x));
+        } else {
+            cerr << '-';
+            print(__uint128_t(-x));
+        }
+    }
+    void print(string x) { cerr << "\"" << x << "\""; }
+    void print(vector<bool> &v) { /* Overloaded this because stl optimizes
+                                     vector<bool> by using _Bit_reference
+                                     instead of bool to conserve space. */
         int f = 0;
-        writer_out << '{';
-        for (auto &&i : v)
-            writer_out << (f++ ? "," : "") << (i ? "T" : "F");
-        writer_out << "}";
+        cerr << '{';
+        for (auto &&i : v) cerr << (f++ ? "," : "") << (i ? "True" : "False");
+        cerr << "}";
     }
-    /* Templates Declarations to support nested datatypes */
-    template <typename T>
-    void print(T &&x);
-    template <typename T>
-    void print(vector<vector<T>> mat);
-    template <typename T, size_t N, size_t M>
-    void print(T (&mat)[N][M]);
-    template <typename F, typename S>
-    void print(pair<F, S> x);
-    template <typename T, size_t N>
-    struct Tuple;
-    template <typename T>
-    struct Tuple<T, 1>;
-    template <typename... Args>
-    void print(tuple<Args...> t);
-    template <typename... T>
-    void print(priority_queue<T...> pq);
-    template <typename T>
-    void print(stack<T> st);
-    template <typename T>
-    void print(queue<T> q);
-    /* Template Datatypes Definitions */
     template <typename T>
     void print(T &&x) {
-        /*  This works for every container that supports range-based loop
-            i.e. vector, set, map, oset, omap, dequeue */
-        int f = 0;
-        writer_out << '{';
-        for (auto &&i : x)
-            writer_out << (f++ ? "," : ""), print(i);
-        writer_out << "}";
+        if constexpr (is_iterable<T>)
+            if (size(x) && is_iterable<decltype(*(
+                               begin(x)))>) { /* Iterable inside Iterable */
+                int f = 0;
+                cerr << "\n~~~~~\n";
+                for (auto &&i : x) {
+                    cerr << setw(2) << left << f++, print(i), cerr << "\n";
+                }
+                cerr << "~~~~~\n";
+            } else { /* Normal Iterable */
+                int f = 0;
+                cerr << "{";
+                for (auto &&i : x) cerr << (f++ ? "," : ""), print(i);
+                cerr << "}";
+            }
+        else if constexpr (requires {
+                               x.pop();
+                           }) /* Stacks, Priority Queues, Queues */
+        {
+            auto temp = x;
+            int f = 0;
+            cerr << "{";
+            if constexpr (requires { x.top(); })
+                while (!temp.empty())
+                    cerr << (f++ ? "," : ""), print(temp.top()), temp.pop();
+            else
+                while (!temp.empty())
+                    cerr << (f++ ? "," : ""), print(temp.front()), temp.pop();
+            cerr << "}";
+        } else if constexpr (requires {
+                                 x.first;
+                                 x.second;
+                             }) /* Pair */
+        {
+            cerr << '(', print(x.first), cerr << ',', print(x.second),
+                cerr << ')';
+        } else if constexpr (requires { get<0>(x); }) /* Tuple */
+        {
+            int f = 0;
+            cerr << '(', apply(
+                             [&f](auto... args) {
+                                 ((cerr << (f++ ? "," : ""), print(args)), ...);
+                             },
+                             x);
+            cerr << ')';
+        } else
+            cerr << x;
     }
-    template <typename T>
-    void print(vector<vector<T>> mat) {
-        int f = 0;
-        writer_out << "\n~~~~~\n";
-        for (auto &&i : mat) {
-            writer_out << setw(2) << left << f++, print(i), writer_out << "\n";
-        }
-        writer_out << "~~~~~\n";
-    }
-    template <typename T, size_t N, size_t M>
-    void print(T (&mat)[N][M]) {
-        int f = 0;
-        writer_out << "\n~~~~~\n";
-        for (auto &&i : mat) {
-            writer_out << setw(2) << left << f++, print(i), writer_out << "\n";
-        }
-        writer_out << "~~~~~\n";
-    }
-    template <typename F, typename S>
-    void print(pair<F, S> x) {
-        writer_out << '(';
-        print(x.first);
-        writer_out << ',';
-        print(x.second);
-        writer_out << ')';
-    }
-    template <typename T, size_t N>
-    struct Tuple {
-        static void printTuple(T t) {
-            Tuple<T, N - 1>::printTuple(t);
-            writer_out << ",", print(get<N - 1>(t));
-        }
-    };
-    template <typename T>
-    struct Tuple<T, 1> {
-        static void printTuple(T t) { print(get<0>(t)); }
-    };
-    template <typename... Args>
-    void print(tuple<Args...> t) {
-        writer_out << "(";
-        Tuple<decltype(t), sizeof...(Args)>::printTuple(t);
-        writer_out << ")";
-    }
-    template <typename... T>
-    void print(priority_queue<T...> pq) {
-        int f = 0;
-        writer_out << '{';
-        while (!pq.empty())
-            writer_out << (f++ ? "," : ""), print(pq.top()), pq.pop();
-        writer_out << "}";
-    }
-    template <typename T>
-    void print(stack<T> st) {
-        int f = 0;
-        writer_out << '{';
-        while (!st.empty())
-            writer_out << (f++ ? "," : ""), print(st.top()), st.pop();
-        writer_out << "}";
-    }
-    template <typename T>
-    void print(queue<T> q) {
-        int f = 0;
-        writer_out << '{';
-        while (!q.empty())
-            writer_out << (f++ ? "," : ""), print(q.front()), q.pop();
-        writer_out << "}";
-    }
-    /* Printer functions */
-    void printer(const char *) {} /* Base Recursive */
     template <typename T, typename... V>
     void printer(const char *names, T &&head, V &&...tail) {
-        /* Using && to capture both lvalues and rvalues */
         int i = 0;
-        for (size_t bracket = 0; names[i] != '\0' and (names[i] != ',' or bracket != 0); i++)
+        for (size_t bracket = 0;
+             names[i] != '\0' and (names[i] != ',' or bracket != 0); i++)
             if (names[i] == '(' or names[i] == '<' or names[i] == '{')
                 bracket++;
             else if (names[i] == ')' or names[i] == '>' or names[i] == '}')
                 bracket--;
-        writer_out.write(names, i) << " = ";
+        cerr.write(names, i) << " = ";
         print(head);
-        if (sizeof...(tail))
-            writer_out << " ||", printer(names + i + 1, tail...);
+        if constexpr (sizeof...(tail))
+            cerr << " ||", printer(names + i + 1, tail...);
         else
-            writer_out << "]\n";
+            cerr << "]\n";
     }
-} // namespace __DEBUG_UTIL__
 
+}  // namespace __DEBUG_UTIL__
 void err_prefix(string func, int line) {
-    writer_out << "\033[0;31m\u001b[1mDEBUG\033[0m: "
+    std::cerr << "\033[0;31m\u001b[1mDEBUG\033[0m: "
                << "\u001b[34m" << func << "\033[0m"
                << ":"
                << "\u001b[34m" << line << "\033[0m: [";
 }
 
 #ifdef CDEBUG
-#define clg(...) err_prefix(__FUNCTION__, __LINE__), __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
+#define clg(...)                        \
+    err_prefix(__FUNCTION__, __LINE__), \
+        __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
 #else
-#define clg(...) writer_out << __func__ << ":" << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
-#endif
-
-/**************************************************2.0************************************************************************************************/
-
-namespace debug_printer {
-    const string WHITE = "\033[0;m";
-    const string RED = "\033[0;31m";
-    const string BLUE = "\033[0;34m";
-    const string GREEN = "\033[0;32m";
-
-    bool colored_output = false;
-    string white = colored_output ? WHITE : "";
-    string outer = colored_output ? RED : "";
-    string var_name = colored_output ? BLUE : "";
-    string var_value = colored_output ? GREEN : "";
-    ostream &operator<<(ostream &os, __uint128_t num) {
-        std::string str;
-        do
-            str += static_cast<char>(num % 10 + 48), num /= 10;
-        while (num > 0);
-        std::reverse(str.begin(), str.end());
-        return os << str;
-    }
-
-    ostream &operator<<(ostream &os, __int128_t num) {
-        const bool neg = num < 0;
-        std::string str;
-        if (num < LONG_MIN) {
-            str.push_back((-(num % 10)) + 48);
-            num /= 10;
-        }
-        if (neg)
-            num = -num;
-        do
-            str += static_cast<char>(num % 10 + 48), num /= 10;
-        while (num > 0);
-        if (neg)
-            str += '-';
-        std::reverse(str.begin(), str.end());
-        return os << str;
-    }
-
-    template <typename T>
-    concept is_iterable = requires(T &&x) { begin(x); } && !is_same_v<remove_cvref_t<T>, string>;
-
-    void print(const char *x) {
-        writer_out << x;
-    }
-    void print(char x) {
-        writer_out << "\'" << x << "\'";
-    }
-    void print(bool x) {
-        writer_out << (x ? "T" : "F");
-    }
-    void print(string x) {
-        writer_out << "\"" << x << "\"";
-    }
-
-    void print(vector<bool> &&v) {
-        /* Overloaded this because stl optimizes vector<bool> by using
-           _Bit_reference instead of bool to conserve space. */
-        int f = 0;
-        writer_out << "{";
-        for (auto &&i : v) {
-            writer_out << (f++ ? "," : "") << (i ? "T" : "F");
-        }
-        writer_out << "}";
-    }
-
-    template <typename T>
-    void print(T &&x) {
-        if constexpr (is_iterable<T>) {
-            if (size(x) && is_iterable<decltype(*(begin(x)))>) {
-                /* Iterable inside Iterable */
-                int f = 0;
-                int w = max(0, (int)log10(size(x) - 1)) + 2;
-                writer_out << "\n~~~~~\n";
-                for (auto &&i : x) {
-                    writer_out << setw(w) << left << f++, print(i), writer_out << "\n";
-                }
-                writer_out << "~~~~~\n";
-            } else {
-                /* Normal Iterable */
-                int f = 0;
-                writer_out << "{";
-                for (auto &&i : x) {
-                    writer_out << (f++ ? "," : ""), print(i);
-                }
-                writer_out << "}";
-            }
-        } else if constexpr (requires { x.pop(); }) {
-            /* Stacks, Queues, Priority Queues */
-            auto temp = x;
-            int f = 0;
-            writer_out << "{";
-            if constexpr (requires { x.top(); }) {
-                while (!temp.empty()) {
-                    writer_out << (f++ ? "," : ""), print(temp.top()), temp.pop();
-                }
-            } else {
-                while (!temp.empty()) {
-                    writer_out << (f++ ? "," : ""), print(temp.front()), temp.pop();
-                }
-            }
-            writer_out << "}";
-        } else if constexpr (requires { x.first; x.second; }) {
-            /* Pair */
-            writer_out << "(", print(x.first), writer_out << ",", print(x.second), writer_out << ")";
-        } else if constexpr (requires { get<0>(x); }) {
-            /* Tuple */
-            int f = 0;
-            writer_out << "(";
-            apply([&f](auto... args) {
-                ((writer_out << (f++ ? "," : ""), print(args)), ...);
-            },
-                  x);
-            writer_out << ")";
-        } else {
-            writer_out << x;
-        }
-    }
-
-    template <typename T, typename... V>
-    void printer(const char *names, T &&head, V &&...tail) {
-        int i = 0;
-        for (size_t bracket = 0; names[i] != '\0' and (names[i] != ',' or bracket != 0); i++) {
-            if (names[i] == '(' or names[i] == '<' or names[i] == '{') {
-                bracket++;
-            } else if (names[i] == ')' or names[i] == '>' or names[i] == '}') {
-                bracket--;
-            }
-        }
-        writer_out << var_name;
-        writer_out.write(names, i) << outer << " = " << var_value;
-        print(head);
-        if constexpr (sizeof...(tail)) {
-            writer_out << outer << " ||", printer(names + i + 1, tail...);
-        } else {
-            writer_out << outer << "]\n"
-                       << white;
-        }
-    }
-} // namespace debug_printer
-
-#if defined(CDEBUG)
-#define dbg(...) writer_out << __func__ << ":" << __LINE__ << ": [", debug_printer::printer(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define dbg(...)
+#define clg(...)                                        \
+    writer_out << __func__ << ":" << __LINE__ << ": [", \
+        __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
 #endif
