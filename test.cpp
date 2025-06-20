2729bd3b984a496ebdb086b5c7a40263b592a03d
saurav-77 <srvkrrr@gmail.com>
2025-06-20 17:54:37 +0000

    .

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using uint = uint32_t;
+using ll = int64_t;
+using ull = uint64_t;
+using lll = __int128_t;
+using ulll = __uint128_t;
+
+#if defined(LOCAL)
+#include "__debug.h"
+#else
+#define clg(...)
+#endif
+
+void solve() {
+    
+}
+
+int main() {
+    int tc = 1;
+    cin >> tc;
+    for (int i = 1; i <= tc; ++i) {
+        solve();
+    }
+    return 0;
+}
