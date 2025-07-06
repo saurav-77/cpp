961e15f8876341ac2b506f5ac74f194a02b40420
saurav-77 <srvkrrr@gmail.com>
2025-07-06 16:23:46 +0000

    .

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = int64_t;
+using ull = uint64_t;
+using lll = __int128_t;
+using ulll = __uint128_t;
+
+#if defined(LOCAL)
+#include "__debug.h"
+#else
+#define clg(...)
+#endif
+
+void solve() {
+    
+}
+
+int32_t main() {
+#ifdef CDEBUG
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "w", stderr);
+#endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int i = 1; i <= tc; ++i) {
+        solve();
+    }
+    return 0;
+}
