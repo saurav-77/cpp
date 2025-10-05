a8a34417e7f0db1ca6b8c637e61a836f02def933
saurav-77 <srvkrrr@gmail.com>
2025-10-05 09:56:20 +0000

    .

--- codechef.h
+++ codechef.h
@@ -168,11 +168,6 @@ void solve() {
 
 int32_t main() {
     ios_base::sync_with_stdio(false), cin.tie(nullptr);
-#ifdef CDEBUG
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    // freopen("error.txt", "w", stderr);
-#endif
     int tc = 1;
     cin >> tc;
     for (int i = 1; i <= tc; ++i) {
